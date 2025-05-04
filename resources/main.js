5a50964d7f7f50d7f4ce3098ad5f5ec611ec2a30
kurtchinta <blue09908@gmail.com>
2025-05-04 13:06:54 +0800

    kk

--- resources/main.js
+++ resources/main.js
@@ -1,29 +1,4 @@
-import { createApp, defineAsyncComponent } from 'vue'
+import { createApp } from 'vue'
+import App from './App.vue'
 
-const app = createApp({
-  template: `
-    <div>
-      <component 
-        v-for="(Comp, name) in pages" 
-        :is="Comp" 
-        :key="name" 
-      />
-    </div>
-  `,
-  data() {
-    return {
-      pages: {}
-    }
-  },
-  async created() {
-    const modules = import.meta.glob('./pages/*.vue')
-    const loadedPages = {}
-    for (const path in modules) {
-      const name = path.split('/').pop().replace('.vue', '')
-      loadedPages[name] = defineAsyncComponent(modules[path])
-    }
-    this.pages = loadedPages
-  }
-})
-
-app.mount('#app')
+createApp(App).mount('#app')
