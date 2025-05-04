f80319e523ef4415721397b768b3051c92c4bb53
kurtchinta <blue09908@gmail.com>
2025-05-04 11:54:58 +0800

    papa

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  root: 'resources',
+  root: '.',
   plugins: [vue()],
   build: {
     outDir: '../dist',
